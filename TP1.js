236abe0e4749ebf6bf4f098d7cb42329c99da289
Mirado14112003 <razafimandimbymirado@gmail.com>
2026-01-22 14:16:02 +0300

    Ajout Once

--- TP1.js
+++ TP1.js
@@ -1,1 +1,13 @@
+function once(fn) {
+    let called = false; // variable privée qui mémorise si fn a été appelée
+    let result; //variable privée pour mémoriser le résultat du premier appel
+
+    return function (...args) { //retourne une nouvelle fonction
+        if (!called) { //si la fonction n'a jamais été appelé
+            called = true;
+            result = fn(...args);
+        }
+        return result;
+    };
+}
 
