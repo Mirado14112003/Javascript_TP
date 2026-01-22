bdf906175981a2d20aa9968a8fbb9a6e1265730f
unknown <you@example.com>
2026-01-22 14:12:16 +0300

    new

--- TP1.js
+++ TP1.js
@@ -1,1 +1,15 @@
-
+//closure & fonctions avancées (incrementation , decrementation , et valeur)
+function creerCompteur(){
+    let valeur = 0;
+    return {
+        incrementer: function(){
+            valeur++;
+        },
+        decrementer: function(){
+            valeur--;
+        },
+        Valeur: function(){
+            return valeur;
+        }
+    };
+}
